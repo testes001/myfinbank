b82215d51503506a302f6a05ed27d39238d074a9
testes001 <washed-elk-untrue@duck.com>
2025-12-26 10:23:50 +0000

    yj

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from '@tanstack/react-router';
 import { EnhancedLoginForm } from '@/components/EnhancedLoginForm';
 
 export const Route = createFileRoute('/login')({
-  component: EnhancedLoginForm,
+  component: () => <EnhancedLoginForm mode="login" />,
 });
